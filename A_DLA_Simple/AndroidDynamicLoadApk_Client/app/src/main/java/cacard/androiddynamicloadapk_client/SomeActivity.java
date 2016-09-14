076a7afc61823becc5f87eaaf6513e658c9c0e2b
cacard <cacard@126.com>
2016-09-14 17:20:51 +0800

    动态创建的View显示成功
    基本原理走通  http://blog.csdn.net/singwhatiwanna/article/details/22597587

--- A_DLA_Simple/AndroidDynamicLoadApk_Client/app/src/main/java/cacard/androiddynamicloadapk_client/SomeActivity.java
+++ A_DLA_Simple/AndroidDynamicLoadApk_Client/app/src/main/java/cacard/androiddynamicloadapk_client/SomeActivity.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 /**
  * 这个直接使用动态创建的控件，而不是使用资源布局文件。
  * 宿主调起后应该可以正常显示。
- *
+ * <p>
  * Created by cunqingli on 2016/9/13.
  */
 public class SomeActivity extends BaseActivity {
@@ -16,8 +16,8 @@ public class SomeActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setTitle("Client/SomeActivity");
 
-        LinearLayout ll = new LinearLayout(this);
-        TextView tv = new TextView(this);
+        LinearLayout ll = new LinearLayout(mProxy); // NOT 'this'
+        TextView tv = new TextView(mProxy);
         tv.setText("Clinet->SomeActivity");
 
         ll.addView(tv);
--- A_DLA_Simple/AndroidDynamicLoadApk_SimpleDemo/app/src/main/java/cacard/androiddynamicloadapk_simpledemo/MainActivity.java
+++ A_DLA_Simple/AndroidDynamicLoadApk_SimpleDemo/app/src/main/java/cacard/androiddynamicloadapk_simpledemo/MainActivity.java
@@ -12,17 +12,19 @@ import android.widget.TextView;
  * 这个Activity是测试入口，他是宿主（Host）的普通Activity，
  * 不觉察到插件（Client）的任何信息。
  * 插件的调起完全交给Proxy（ProxyActivity）。
- * <p>
+ * <p/>
  * Created by cunqingli on 2016/9/13.
  */
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    int btnId = 0;
+    int btnId1 = 0;
+    int btnId2 = 0;
 
     /**
      * 要调起的Client中哪个Activity？使用包路名名
      */
-    static final String targetActivityInClient = "cacard.androiddynamicloadapk_client.SomeActivity";
+    static final String targetActivityInClient1 = "cacard.androiddynamicloadapk_client.SomeActivity";
+    static final String targetActivityInClient2 = "cacard.androiddynamicloadapk_client.MainActivity";
 
     // test
     public MainActivity() {
@@ -35,12 +37,20 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
 
         LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
 
         Button btn = new Button(this);
-        btn.setId(btnId = View.generateViewId());
-        btn.setText("Invoke Plugin's Activity");
+        btn.setId(btnId1 = View.generateViewId());
+        btn.setText("Invoke Plugin's Activity:SomeActivity Using code View");
         btn.setOnClickListener(this);
+
+        Button btn2 = new Button(this);
+        btn2.setId(btnId2 = View.generateViewId());
+        btn2.setText("Invoke Plugin's Activity:MainActivity Using xml");
+        btn2.setOnClickListener(this);
+
         ll.addView(btn);
+        ll.addView(btn2);
 
         setContentView(ll);
     }
@@ -48,12 +58,21 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         if (view != null) {
-            if (view.getId() == btnId) {
-                MyLog.log("clicked. will start activity:" + targetActivityInClient);
-                Intent intent = new Intent(MainActivity.this, ProxyActivity.class);
-                intent.putExtra(DLConfig.KEY_CLIENT_ACTIVITY_NAME, targetActivityInClient);
-                MainActivity.this.startActivity(intent);
+
+            String target = "";
+
+            if (view.getId() == btnId1) {
+                target = targetActivityInClient1;
             }
+
+            if (view.getId() == btnId2) {
+                target = targetActivityInClient2;
+            }
+
+            MyLog.log("clicked. will start activity:" + target);
+            Intent intent = new Intent(MainActivity.this, ProxyActivity.class);
+            intent.putExtra(DLConfig.KEY_CLIENT_ACTIVITY_NAME, target);
+            MainActivity.this.startActivity(intent);
         }
     }
 }
